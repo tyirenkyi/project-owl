77e9b4ba5a73d0212aef96eb1a77e8696868e941
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2021-05-31 00:02:10 +0000

    Upload modal UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 
 import "../assets/css/nav.css";
 import uploadIcon from "../assets/images/upload.svg";
+import Upload from "./upload";
 
 const Nav = () => {
+  const [showUploadModal, setShowUploadModal] = useState<boolean>(false);
+
+  const toggleUploadModal = () => {
+    if(showUploadModal)
+      enableScroll()
+
+    if(!showUploadModal)
+      disableScroll()
+    
+    setShowUploadModal((prevValue) => !prevValue);
+  }
+
+  const enableScroll = () => {
+    const body = document.body;
+    body.classList.remove('no-scroll');
+  }
+
+  const disableScroll = () => {
+    const body = document.body;
+    body.classList.add('no-scroll')
+  }
+
   return (
     <div className="nav-container">
       <h2>Project OWL</h2>
@@ -14,7 +38,7 @@ const Nav = () => {
         <button className="clear-nav-btn">
           Issues <FaChevronDown />
         </button>
-        <button className="upload-btn">
+        <button className="upload-btn" onClick={toggleUploadModal}>
           <img src={uploadIcon} alt="upload icon" />
           Upload Recording
         </button>
@@ -24,6 +48,7 @@ const Nav = () => {
           <button>Airtime</button>
         </div>
       </div>
+      {showUploadModal && <Upload closeModal={toggleUploadModal}/>}
     </div>
   )
 }
