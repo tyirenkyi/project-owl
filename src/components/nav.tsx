b9aba02df8b3dc9a25123bc710e3da5622dcef27
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2021-06-07 10:12:04 +0000

    Update Nav UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createRef, useEffect, useCallback } from "react";
 import { FaChevronDown } from "react-icons/fa";
 
 import "../assets/css/nav.css";
@@ -7,6 +7,8 @@ import Upload from "./upload";
 
 const Nav = () => {
   const [showUploadModal, setShowUploadModal] = useState<boolean>(false);
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const dropdown = createRef<HTMLDivElement>();
 
   const toggleUploadModal = () => {
     if(showUploadModal)
@@ -28,6 +30,15 @@ const Nav = () => {
     body.classList.add('no-scroll')
   }
 
+  const focusDropdown = useCallback(() => {
+    if(showDropdown)
+      dropdown.current?.focus();
+  }, [dropdown, showDropdown])
+
+  useEffect(() => {
+    focusDropdown();
+  }, [showDropdown, focusDropdown])
+
   return (
     <div className="nav-container">
       <h2>Project OWL</h2>
@@ -35,18 +46,25 @@ const Nav = () => {
         <button className="clear-nav-btn">
           About
         </button>
-        <button className="clear-nav-btn">
+        <button className="clear-nav-btn" onClick={() => setShowDropdown(true)}>
           Issues <FaChevronDown />
         </button>
         <button className="upload-btn" onClick={toggleUploadModal}>
           <img src={uploadIcon} alt="upload icon" />
           Upload Recording
         </button>
-        <div className="issues-div">
-          <button>Internet Connectivity</button>
-          <button>Data</button>
-          <button>Airtime</button>
-        </div>
+        {showDropdown && (
+          <div 
+            className="issues-div" 
+            tabIndex={1} 
+            onBlur={() => setShowDropdown(false)}
+            ref={dropdown}
+          >
+            <button>Internet Connectivity</button>
+            <button>Data</button>
+            <button>Airtime</button>
+          </div>
+        )}
       </div>
       {showUploadModal && <Upload closeModal={toggleUploadModal}/>}
     </div>
